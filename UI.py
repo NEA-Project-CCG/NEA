20288bc087e9ea3cb1d446f74d4e6777dd08f5f3
NEA-Project-CCG <19es9954@pupil.uvhs.org.uk>
2025-11-04 15:21:44 +0000

    Update UI.py

--- UI.py
+++ UI.py
@@ -13,8 +13,32 @@ def initial_UI():
 
     pygame.display.flip()
 
+    return window
+
 def draw_window(window, state):
-    
+
+    if state == 0:
+        window.fill((0,0,0))
+        pygame.draw.rect(window, (255,255,255), pygame.Rect(0,0,350, 400))
+        pygame.draw.rect(window, (255,0,0), pygame.Rect(0,425,350, 175))
+        pygame.draw.rect(window, (0,255,0), pygame.Rect(375,0,425, 600))
+        
+    if state == 1:
+        window.fill((255, 255, 255))
+        pygame.draw.rect(window, (41, 103, 204), pygame.Rect(10, 10, 30, 30))
+        
+    if state == 2:
+        window.fill((255, 0, 0))
+        pygame.draw.rect(window, (41, 103, 204), pygame.Rect(10, 10, 30, 30))
+        
+    if state == 3:
+        window.fill((0, 255, 0))
+        pygame.draw.rect(window, (41, 103, 204), pygame.Rect(10, 10, 30, 30))
+        
+        
+        
+    pygame.display.flip()
+
 
 
 
