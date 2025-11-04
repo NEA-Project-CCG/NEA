b3a5ac0477c987f91c8ad31ab06fc0f2dc2603de
NEA-Project-CCG <19es9954@pupil.uvhs.org.uk>
2025-11-04 15:22:35 +0000

    Update Gameloop.py

--- Gameloop.py
+++ Gameloop.py
@@ -1,16 +1,15 @@
 import pygame
-
-from UI import initial_UI
+from UI import initial_UI, draw_window
 
 
 
 def gameloop():
 
+    state = 0
 
 
 
-
-    initial_UI()
+    window = initial_UI()
 
     running = True
     while running:
@@ -39,9 +38,33 @@ def gameloop():
 
             if event.type == pygame.MOUSEBUTTONDOWN:
                 pos = pygame.mouse.get_pos()
-                print(pos)
-                type = pygame.mouse.get_pressed()[0]
-                print(type)
+                pos_x = pos[0]
+                pos_y = pos[1]
+                
+     
+
+                Mtype = pygame.mouse.get_pressed()[0]
+                
+                if state == 0:
+                    if Mtype == 1:
+                        if pos_x >= 0 and pos_x < 350 and pos_y >= 0 and pos_y < 400:
+                            state = 1
+               
+                            
+                        elif pos_x >= 0 and pos_x < 350 and pos_y >= 425 and pos_y <= 600:
+                            state = 2
+                            
+                        elif pos_x >= 375 and pos_x < 800 and pos_y >= 0 and pos_y <= 600:
+                            state = 3
+                        
+                elif state != 0:
+                    if Mtype == 1:
+                        if pos_x >= 10 and pos_y >= 10 and pos_x <= 40 and pos_y <= 40:
+                            state = 0
+
+                    
+                    
+                        
 
 
 
@@ -54,4 +77,14 @@ def gameloop():
 
 
 
+
+        draw_window(window, state)
+        
+        
+        
+        
+        
+        
+if __name__ == '__main__':
+    gameloop()
 
