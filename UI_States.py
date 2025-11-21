d67afb5b24ddbd24b7003844ad863ca2b7c8d026
Ewan Stamp <19es9954@pupil.uvhs.org.uk>
2025-11-21 15:26:04 +0000

    Update UI_States.py

--- UI_States.py
+++ UI_States.py
@@ -1,26 +1,29 @@
-from tkinter import font
+import Campaign_logic
 
 import pygame
 
 class ui_states:
     @staticmethod
     def select_state(window, state, font):
-        if state == 0:
-            ui_states.__state_0(window, font)
+        if state == "hub":
+            ui_states.__state_hub(window, font)
 
-        if state == 1:
-            ui_states.__state_1(window)
+        if state == "characters":
+            ui_states.__state_characters(window)
 
-        if state == 2:
-            ui_states.__state_2(window)
+        if state == "journey guide":
+            ui_states.__state_journey_guide(window)
 
-        if state == 3:
-            ui_states.__state_3(window, font)
+        if state == "campaigns":
+            ui_states.__state_campaigns(window, font)
+
+        if state == "campaign_1":
+            ui_states.__state_campaign_1(window, font)
 
         return window
 
     @staticmethod
-    def __state_0(window, font):
+    def __state_hub(window, font):
         window.fill((0, 0, 0))
         pygame.draw.rect(window, (0, 0, 255), pygame.Rect(0, 0, 350, 400))
         text = ("Characters")
@@ -41,7 +44,7 @@ class ui_states:
 
     #Characters screen
     @staticmethod
-    def __state_1(window):
+    def __state_characters(window):
         window.fill((0, 0, 255))
         pygame.draw.rect(window, (41, 103, 204), pygame.Rect(10, 10, 30, 30))
 
@@ -49,7 +52,7 @@ class ui_states:
 
     #Journey Guide screen
     @staticmethod
-    def __state_2(window):
+    def __state_journey_guide(window):
         window.fill((255, 0, 0))
         pygame.draw.rect(window, (41, 103, 204), pygame.Rect(10, 10, 30, 30))
 
@@ -57,7 +60,7 @@ class ui_states:
 
     #Campaigns screen
     @staticmethod
-    def __state_3(window, font):
+    def __state_campaigns(window, font):
         window.fill((0, 255, 0))
         #Draws back button
         pygame.draw.rect(window, (41, 103, 204), pygame.Rect(10, 10, 30, 30))
@@ -70,3 +73,32 @@ class ui_states:
 
         return window
 
+    @staticmethod
+    def __state_campaign_1(window, font):
+        window.fill((7, 34, 166))
+
+        # Draws back button
+        pygame.draw.rect(window, (41, 103, 204), pygame.Rect(10, 10, 30, 30))
+
+        # Draws campaign background
+        pygame.draw.rect(window, (255, 125, 125), pygame.Rect(10, 100, 780, 490))
+
+        pos_x = 55
+        pos_y = [450, 225]
+
+        for i in range(10):
+            # draws campaign nodes
+            pygame.draw.rect(window, (41, 255, 20), pygame.Rect(pos_x, pos_y[i%2], 40, 40))
+            text = str(i + 1)
+            text = font.render(text, True, (255, 255, 255))
+            window.blit(text, (pos_x, pos_y[i%2]))
+
+            pos_x += 70
+
+
+
+
+
+
+
+        return window
