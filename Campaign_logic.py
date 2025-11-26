90f871756fd2ba063cbbb53976e14d299f0773b7
Ewan Stamp <19es9954@pupil.uvhs.org.uk>
2025-11-26 11:50:29 +0000

    Update Campaign_logic.py

--- Campaign_logic.py
+++ Campaign_logic.py
@@ -1,5 +1,6 @@
 import sqlite3
 from Multiplier_Calculator import Multiplier
+from UI_States import ui_states
 from Ninjago_Characters import *
 from Chima_Characters import *
 
@@ -50,14 +51,72 @@ class Campaign_Logic:
 
         multiplier = Multiplier.calculate_stats_enemy(Enemy_battle[5], Enemy_battle[6], Enemy_battle[7])
 
-        Ofile = open("All_chars_list.txt", "r")
-        all_chars = Ofile.readline().strip()
-        all_chars = all_chars.split(",")
-
-        Enemy_Character_1 = all_chars[Enemy_battle[0]]
-
-
+        Echaracter1 = Campaign_Logic.__init_char(Enemy_battle[0], multiplier)
+        Echaracter2 = Campaign_Logic.__init_char(Enemy_battle[1], multiplier)
+        Echaracter3 = Campaign_Logic.__init_char(Enemy_battle[2], multiplier)
+        Echaracter4 = Campaign_Logic.__init_char(Enemy_battle[3], multiplier)
+        Echaracter5 = Campaign_Logic.__init_char(Enemy_battle[4], multiplier)
 
 
-if __name__ == '__main__':
-    Campaign_Logic.start_battle(None)
+
+
+
+    @staticmethod
+    def __init_char(character_id, multiplier):
+        if character_id == 0:
+            character = Cragger(stat_multiplier=multiplier)
+
+        if character_id == 1:
+            character = Gorzan(stat_multiplier=multiplier)
+
+        if character_id == 2:
+            character = Eris(stat_multiplier=multiplier)
+
+        if character_id == 3:
+            character = Razar(stat_multiplier=multiplier)
+
+        if character_id == 4:
+            character = Rinona(stat_multiplier=multiplier)
+
+        if character_id == 5:
+            character = Laval(stat_multiplier=multiplier)
+
+        if character_id == 6:
+            character = Kai(stat_multiplier=multiplier)
+
+        if character_id == 7:
+            character = Zane(stat_multiplier=multiplier)
+
+        if character_id == 8:
+            character = Cole(stat_multiplier=multiplier)
+
+        if character_id == 9:
+            character = Jay(stat_multiplier=multiplier)
+
+        if character_id == 10:
+            character = Lloyd(stat_multiplier=multiplier)
+
+        if character_id == 11:
+            character = Wu(stat_multiplier=multiplier)
+
+        return character
+
+    @staticmethod
+    def __Player_choose_chars(Player_id):
+        conn = sqlite3.connect('NEA Database.db')
+        cursor = conn.cursor()
+
+        cursor.execute("""SELECT Name FROM General_Character, Player_Table;
+                       WHERE Player_Table.Player_id = ?;
+                       AND General_Character.Character_id = Player_Table.Character_id;
+                       AND Player_Table.Collected = True;
+                       ORDER BY Level DESC;""",
+                       (Player_id,))
+
+        names = cursor.fetchall()
+
+        ui_states.Character_selection_screen(names)
+
+
+
+
