cc4c7fd1898ac2eb4e0547a065cb78f9d49956e1
Ewan Stamp <19es9954@pupil.uvhs.org.uk>
2025-12-09 15:35:24 +0000

    Add files via upload

---
+++ Pygame_clicks_logic.py
@@ -0,0 +1,91 @@
+import pygame
+import re
+from Campaign_logic import Campaign_Logic
+
+
+class LMB:
+
+    @staticmethod
+    def clicked_state(state, campaign_re, pos_x, pos_y):
+        if state == "hub":
+            state = LMB.in_hub(pos_x, pos_y, state)
+        elif state in ["journey guide", "campaigns", "characters"]:
+            state = LMB.in_hub_level_1(pos_x, pos_y, state)
+        elif re.match(campaign_re, state):
+            state = LMB.inside_a_campaign(pos_x, pos_y, state)
+        return state
+
+
+
+    @staticmethod
+    def in_hub(pos_x, pos_y, state):
+        if pos_x >= 0 and pos_x < 350 and pos_y >= 0 and pos_y < 400:
+            state = "characters"
+
+
+        elif pos_x >= 0 and pos_x < 350 and pos_y >= 425 and pos_y <= 600:
+            state = "journey guide"
+
+        elif pos_x >= 375 and pos_x < 800 and pos_y >= 0 and pos_y <= 600:
+            state = "campaigns"
+
+
+        return state
+
+    @staticmethod
+    def in_hub_level_1(pos_x, pos_y, state):
+        if pos_x >= 10 and pos_y >= 10 and pos_x <= 40 and pos_y <= 40:
+            state = "hub"
+
+        elif state == "campaigns":
+            state = LMB.in_campaigns(pos_x, pos_y, state)
+
+        return state
+
+
+    @staticmethod
+    def in_campaigns(pos_x, pos_y, state):
+        if pos_x >= 40 and pos_y >= 60 and pos_x <= 290 and pos_y <= 540:
+            state = "campaign_1_1"
+
+        return state
+
+
+    @staticmethod
+    def inside_a_campaign(pos_x, pos_y, state):
+        if pos_x >= 10 and pos_y >= 10 and pos_x <= 40 and pos_y <= 40:
+            state = "campaigns"
+
+        if pos_y >= 50 and pos_y <= 100:
+            state = LMB.change_chapter(pos_x, state)
+
+        if (pos_y >= 225 and pos_y <= 265) or (pos_y >= 450 and pos_y <= 490):
+            state = LMB.campaign_node(pos_x, state)
+
+
+
+        return state
+
+    @staticmethod
+    def change_chapter(pos_x, state):
+        if pos_x <= 260 and pos_x >= 0:
+            state = state[:11] + f"1"
+        elif pos_x >= 270 and pos_x <= 530:
+            state = state[:11] + f"2"
+        elif pos_x >= 540 and pos_x <= 800:
+            state = state[:11] + f"3"
+
+        return state
+
+    @staticmethod
+    def campaign_node(pos_x, state):
+        for i in range(10):
+            if pos_x >= (70 * (i) + 55) and pos_x <= (70 * (i) + 95):
+                if len(state) < len("campaign_x_x_"):
+                    state += f"_{i}"
+                elif state[len(state) - 1] != f"{i}":
+                    state = state[:(len(state) - 1)] + f"{i}"
+
+        return state
+
+
