e294ab881b9c2b1353950ecedc8b3f1200170663
Ewan Stamp <19es9954@pupil.uvhs.org.uk>
2025-12-19 10:42:14 +0000

    Update Battle_Loop.py

--- Battle_Loop.py
+++ Battle_Loop.py
@@ -1,7 +1,8 @@
-import Battle_Classes
-from UI import draw_battle
+from UI import draw_battle, Draw_abilities
 from Battle_Classes import *
 from Battle_Queue import *
+import pygame
+from Pygame_clicks_logic import LMB
 
 class Battle_Loop:
 
@@ -14,8 +15,11 @@ class Battle_Loop:
         queue.speed_turn()
 
         char_turn = queue.Get_character_for_turn()
+        Battle_Loop.__turn_loop(char_turn, window)
 
-        print(char_turn.character.GetName())
+
+
+
 
 
 
@@ -85,5 +89,25 @@ class Battle_Loop:
 
         return Battle, Team1, Team2, list_of_chars
 
+
+    @staticmethod
+    def __turn_loop(character, window):
+        basic = character.basic
+        special = character.special
+        ultimate = character.ultimate
+
+        attacked = False
+
+        while not attacked:
+            window = Draw_abilities(window)
+
+            lmb = 1
+
+            for event in pygame.event.get():
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    if pygame.mouse.get_pressed()[0]:
+                        pos_x = pygame.mouse.get_pos()[0]
+                        pos_y = pygame.mouse.get_pos()[1]
+                        ability = LMB.click_attack(pos_x, pos_y)
 
 
