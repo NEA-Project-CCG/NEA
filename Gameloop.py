478cab5686e6cda1d47be6e1c96a6af18774d8c8
Ewan Stamp <19es9954@pupil.uvhs.org.uk>
2026-02-10 15:07:57 +0000

    Update Gameloop.py

--- Gameloop.py
+++ Gameloop.py
@@ -1,8 +1,9 @@
 import pygame
-from UI import initial_UI, draw_window
+from UI import initial_UI, draw_window, draw_win, draw_loss
 from Campaign_logic import Campaign_Logic
 from Pygame_clicks_logic import LMB
 from Battle_Loop import Battle_Loop
+from Journey_logic import Journeys
 
 
 class Gameloop:
@@ -14,6 +15,7 @@ class Gameloop:
         state = "hub"
 
         battling = False
+        Journey = False
 
         lmb = 1
 
@@ -32,12 +34,41 @@ class Gameloop:
                     if pygame.mouse.get_pressed()[0]:
                         pos_x = pygame.mouse.get_pos()[0]
                         pos_y = pygame.mouse.get_pos()[1]
-                        state = LMB.clicked_state(state, campaign_re, pos_x, pos_y)
+                        state = LMB.clicked_state(state, campaign_re, pos_x, pos_y, Player_id)
                         if len(state) == 14:
                             comp, Battle_id = Campaign_Logic.Check_stage(state[Campaign_index], state[Chapter_index], state[Stage_index], Player_id)
                             if comp:
                                 Characters = Campaign_Logic.start_battle(Battle_id, Player_id)
                                 battling = True
+
+                        elif state[len(state)-3] == "J":
+                            if state[0] == "L":
+                                journey_id = 2
+                                Journeys.check_eligibility(Player_id, journey_id)
+                                comp, Battle_id = Campaign_Logic.Check_stage(journey_id, 1, state[len(state)-1], Player_id)
+                            elif state[0] == "W":
+                                journey_id = 3
+                                Journeys.check_eligibility(Player_id, journey_id)
+                                comp, Battle_id = Campaign_Logic.Check_stage(journey_id, 1, state[len(state) - 1],
+                                                                             Player_id)
+                            elif state[0] == "D":
+                                journey_id = 4
+                                Journeys.check_eligibility(Player_id, journey_id)
+                                comp, Battle_id = Campaign_Logic.Check_stage(journey_id, 1, state[len(state) - 1],
+                                                                             Player_id)
+                            elif state[0] == "M":
+                                journey_id = 5
+                                Journeys.check_eligibility(Player_id, journey_id)
+                                comp, Battle_id = Campaign_Logic.Check_stage(journey_id, 1, state[len(state) - 1],
+                                                                             Player_id)
+                            if comp:
+                                Characters = Journeys.start_battle(Battle_id, Player_id, journey_id)
+                                Journey = True
+                                battling = True
+
+                            else:
+                                state = state[:len(state)-2]
+
 
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -48,11 +79,29 @@ class Gameloop:
 
                 if battling:
 
-                    Battle, Team1, Team2, list_of_chars, queue = Battle_Loop.Startup(window, Characters)
+                    Battle, Team1, Team2, list_of_chars= Battle_Loop.Startup(window, Characters)
                     while battling:
-                        battling = Battle_Loop.the_loop(window, Battle, Team1, Team2, list_of_chars, queue, Characters)
+                        battling, victory, list_of_chars = Battle_Loop.the_loop(window, Battle, Team1, Team2, list_of_chars)
+
+                        if not battling:
+                            if victory:
+                                if Journey:
+                                    Journey = False
+                                    Journeys.end_battle(Battle_id, Player_id, journey_id)
+                                    state = state[:len(state) - 2]
+
+                                else:
+
+                                    Campaign_Logic.end_battle(Player_id, Characters, Battle_id)
+                                draw_win(window)
+                            else:
+                                if Journey:
+                                    state = state[:len(state) - 2]
+                                Journey = False
+                                draw_loss(window)
+
                 else:
-                    draw_window(window, state, font, campaign_re)
+                    draw_window(window, state, font, campaign_re, Player_id)
 
         
         
