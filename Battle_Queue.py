d82e364efffd9a44f788e6bc8fe986e1df3dd640
Ewan Stamp <19es9954@pupil.uvhs.org.uk>
2026-02-10 15:06:23 +0000

    Update Battle_Queue.py

--- Battle_Queue.py
+++ Battle_Queue.py
@@ -1,6 +1,6 @@
 class Speed_Queue:
     def __init__(self, characters):
-        self.__chars = []
+        self._chars = []
         self.__set_chars(characters)
         self.__set_speeds()
 
@@ -9,23 +9,23 @@ class Speed_Queue:
 
     def __set_chars(self, characters):
         for i in range(10):
-            self.__chars.append([characters[i], True])
+            self._chars.append([characters[i], True])
 
 
 
     def __set_speeds(self):
-        self.__speeds = []
-        for i in range(len(self.__chars)):
-            speed = [self.__chars[i][0].character._speed, i]
-            self.__speeds.append(speed)
+        self._speeds = []
+        for i in range(len(self._chars)):
+            speed = [self._chars[i][0].character._speed, i]
+            self._speeds.append(speed)
 
 
     def __priority(self):
-        self.__speeds.sort(reverse=True)
+        self._speeds.sort(reverse=True)
 
     def speed_turn(self):
-        for i in range(len(self.__chars)):
-            self.__speeds[i][0] += self.__chars[self.__speeds[i][1]][0].character.GetSpeed()
+        for i in range(len(self._chars)):
+            self._speeds[i][0] += self._chars[self._speeds[i][1]][0].character.GetSpeed()
 
 
 
@@ -34,19 +34,31 @@ class Speed_Queue:
         self.__take_turn()
 
 
-    def remove_character(self, index):
+    def remove_character(self, character):
 
-        self.__chars[index][1] = False
+        for i in range(len(self._chars)):
+            if self._chars[i][1] == True:
+                if self._chars[i][0].character.GetHealth() == 0 and self._chars[i][0].character == character.character:
+                    self._chars[i][1] = False
+
+
+
+
+
+
 
 
 
 
     def __take_turn(self):
-        if self.__speeds[0][0] >= 1000:
-            index = self.__speeds[0][1]
-            if self.__chars[index][1] == True:
-                self.__Character_for_turn = self.__chars[index][0]
-                self.__speeds[0][0] = 0
+        if self._speeds[0][0] >= 1000:
+            index = self._speeds[0][1]
+            if self._chars[index][1] == True:
+                self.__Character_for_turn = self._chars[index][0]
+                self._speeds[0][0] = 0
+                self.__priority()
+            else:
+                self._speeds[0][0] = 0
                 self.__priority()
 
         else:
@@ -59,7 +71,7 @@ class Speed_Queue:
 
 
     def GetQueue(self):
-        return self.__chars
+        return self._chars
 
     def Get_character_for_turn(self):
         return self.__Character_for_turn
