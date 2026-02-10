00e387799f52f1914fd1c50c4c4a8dae73de2cc0
Ewan Stamp <19es9954@pupil.uvhs.org.uk>
2026-02-10 15:09:58 +0000

    Update Pygame_clicks_logic.py

--- Pygame_clicks_logic.py
+++ Pygame_clicks_logic.py
@@ -1,3 +1,4 @@
+
 import pygame
 import re
 from Campaign_logic import Campaign_Logic
@@ -15,12 +16,14 @@ class LMB:
             state = LMB.in_hub_level_1(pos_x, pos_y, state, player_id)
         elif re.match(campaign_re, state):
             state = LMB.inside_a_campaign(pos_x, pos_y, state)
+        elif state[len(state) - 1] == "J":
+            state = LMB.in_journey(pos_x, pos_y, state)
         else:
             state = LMB.in_char(pos_x, pos_y, state, player_id)
         return state
 
     @staticmethod
-    def click_attack(pos_x, pos_y):
+    def click_attack(pos_x, pos_y, character):
         start_x = 50
         end_x = 250
         y_top = 560
@@ -28,8 +31,26 @@ class LMB:
 
         if pos_y > y_top and pos_y < y_bottom:
             for i in range(3):
-                if pos_x > start_x + i*250 and pos_x < end_x + i*250:
+                if character.character._special_cooldown > 0 and i == 1:
                     pass
+                elif character.character._ultimate_cooldown > 0 and i == 2:
+                    pass
+                elif pos_x > start_x + i*250 and pos_x < end_x + i*250:
+                    return i
+        return -1
+
+    @staticmethod
+    def click_target(pos_x, pos_y, repeats):
+        start_x = 30
+        end_x = 160
+        y_top = 100
+        y_bottom = 180
+        if pos_y > y_top and pos_y < y_bottom:
+            for i in range(repeats):
+                if pos_x > start_x + i*150 and pos_x < end_x + i*150:
+                    return i
+
+        return -1
 
 
 
@@ -59,6 +80,9 @@ class LMB:
 
         elif state == "characters":
             state = LMB.choosing_char(pos_x,pos_y, state, player_id)
+
+        elif state == "journey guide":
+            state = LMB.in_journeys(pos_x, pos_y, state)
 
         return state
 
@@ -132,5 +156,54 @@ class LMB:
         elif pos_x >= 580 and pos_x <= 660 and pos_y >= 80 and pos_y <= 110:
             char_id = Database.Char_id_from_name(state)
             Char_upgrading.Upgrade_gear(char_id, player_id)
+
+        return state
+
+    @staticmethod
+    def in_journey(pos_x, pos_y, state):
+        if pos_x >= 10 and pos_y >= 10 and pos_x <= 40 and pos_y <= 40:
+            state = "journey guide"
+
+        if pos_y >= 325 and pos_y <= 365:
+            state = LMB.journey_node(pos_x, state)
+
+        return state
+
+
+
+
+    @staticmethod
+    def in_journeys(pos_x, pos_y, state):
+        if pos_x >= 10 and pos_y >= 10 and pos_x <= 40 and pos_y <= 40:
+            state = "hub"
+
+        elif pos_x >= 20 and pos_y >= 60 and pos_x <= 120 and pos_y <= 540:
+            state = "Laval_J"
+
+        elif pos_x >= 130 and pos_y >= 60 and pos_x <= 230 and pos_y <= 540:
+            state = "Wu_J"
+
+        elif pos_x >= 240 and pos_y >= 60 and pos_x <= 340 and pos_y <= 540:
+            state = "Doom_J"
+
+        elif pos_x >= 350 and pos_y >= 60 and pos_x <= 450 and pos_y <= 540:
+            state = "Maul_J"
+
+        elif pos_x >= 460 and pos_y >= 60 and pos_x <= 560 and pos_y <= 540:
+            state = "Superman_J"
+
+        elif pos_x >= 570 and pos_y >= 60 and pos_x <= 670 and pos_y <= 540:
+            state = "Gandalf_J"
+
+        elif pos_x >= 680 and pos_y >= 60 and pos_x <= 780 and pos_y <= 540:
+            state = "Clutch_J"
+
+        return state
+
+    @staticmethod
+    def journey_node(pos_x, state):
+        for i in range(6):
+            if pos_x >= 55 + (120*i) and pos_x <= 95 + (120*i):
+                state += '_' + str(i)
 
         return state
